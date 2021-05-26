813e2198a04510e1d8ced5411643fe8e8747c620
MANISH MOHAPATRA <manishofyore@gmail.com>
2021-05-26 14:22:03 -0400

    Update sspe_comparison.m

--- code-matlab/sspe_comparison.m
+++ code-matlab/sspe_comparison.m
@@ -3,7 +3,7 @@ cd('/Users/manishm/Dropbox (Dartmouth College)/vandermeerlab_tutorial/data/R016-
 % goodGamma: {'R016-2012-10-03-CSC04d.ncs'
 % goodSWR: {'R016-2012-10-03-CSC02b.ncs'}
 % goodTheta: {'R016-2012-10-03-CSC02b.ncs'}
-cfg_in.fc = {'R016-2012-10-03-CSC04d.ncs'};
+cfg_in.fc = {'R016-2012-10-03-CSC02b.ncs'};
 trial_lfp  = LoadCSC(cfg_in);
 evs = LoadEvents([]);
 
@@ -70,57 +70,89 @@ for i = 1:25
     end
     t1 = lfp_prefilt{1}.tvec(t1_idx);
     trial_data{i} = restrict(lfp_prefilt{1}, iv(t1,t2));
-    trial_filt{i} = zeros(length(trial_lfp.label),length(all_phase),length(trial_data{i}.tvec));
-    phase_length = length(trial_data{i}.tvec) - round(length(trial_data{i}.tvec)*0.9);
+    phase_length = length(trial_data{i}.tvec) - round(length(trial_data{i}.tvec)*0.98);
+    trial_filt{i} = zeros(length(trial_lfp.label),length(all_phase),phase_length);
     trial_phase{i} = zeros(length(trial_lfp.label),length(all_phase),phase_length);
     for j = 1:length(all_phase)
         trial_phase{i}(:,j,:) = all_phase{j}(:,seeds(i)+1-phase_length:seeds(i));
-        trial_filt{i}(:,j,:) = lfp_filtered{j}.data(:,t1_idx:seeds(i));
+        trial_filt{i}(:,j,:) = lfp_filtered{j}.data(:,seeds(i)+1-phase_length:seeds(i));
     end
 end
+
+%% Specify various models
+
+% Specify different models
+all_freqs = [2, 7, 19.5, 56, 80];
+all_bands = fstop_list;
+all_sigmaFreqs = [1, 0.1, 0.01, 0.001, 0.0001];
+
+% model specifcations
+model_all.spec = true(1,5);
+model_lo_gamma.spec = [false(1,3), true, false];
+model_hi_gamma.spec = [false(1,4), true];
+model_gamma.spec = [false(1,3), true(1,2)];
+model_no_delta.spec = [false, true(1,4)];
+model_no_theta.spec = [true, false, true(1,3)];
+model_no_beta.spec = [true(1,2), false, true(1,2)];
+model_no_gamma.spec = ~model_gamma.spec;
+model_beta.spec = ~model_no_beta.spec;
+model_theta.spec = ~model_no_theta.spec;
+model_delta.spec = ~model_no_delta.spec;
+model_delta_theta.spec = [true(1,2), false(1,3)];
+model_gamma_delta.spec = model_gamma.spec | model_delta.spec;
+model_gamma_theta.spec = model_gamma.spec | model_theta.spec;
+model_gamma_beta.spec = model_gamma.spec | model_beta.spec;
+model_beta_theta.spec = model_beta.spec | model_theta.spec;
+model_hgamma_theta.spec = model_hi_gamma.spec | model_theta.spec;
+model_lgamma_theta.spec = model_lo_gamma.spec | model_theta.spec;
+
+
+% add more models if necessary
+
 
 %% Use SSPE
-for iD = 1:25
-    for iF = 2%1:length(f_list)
-        this_data = trial_data{iD}.data(1,:);
-        this_filt_data = trial_filt{iD}(1,iF,:);
-        this_filt_phase = trial_phase{iD}(1,iF,:);
-        fig = figure;
-        cfg_sspe.freqs = [2,7,56,80]; % initialization using above not great at identifying starting freq
-        cfg_sspe.Fs = Fs;
-        cfg_sspe.ampVec = [0.99,0.99,0.99,0.99];
-        cfg_sspe.sigmaFreqs = [1,0.1,0.01,0.001];
-        cfg_sspe.sigmaObs = 1;
-        cfg_sspe.window = round(length(this_data)*0.9);
-        cfg_sspe.freqBands = {[2 5], [6.5 9.5] ,[40 65], [70 100]};
-        
-        [omega, phase,phaseBounds, fullX] = causalPhaseEM_MKmdl_temp(this_data, cfg_sspe);
-%         phaseBounds = reshape(permute(phaseBounds,[2,1,3]), size(phaseBounds,1) * size(phaseBounds,2),size(phaseBounds,3));
-%         [omega, ampEst, allQ, R, stateVec, stateCov] = fit_MKModel_multSines(this_data, ...
-%                                     cfg_sspe.freqs, cfg_sspe.Fs, ...
-%                                     cfg_sspe.ampVec, cfg_sspe.sigmaFreqs, ...
-%                                     cfg_sspe.sigmaObs);
-%         phase = angle(stateVec(3, :) + 1i*stateVec( 4, :));
 
-        % TODO: FIX PLotting and Loops
-        
-        wsize = 512;
-        [Pxx, F] = pwelch(this_data, rectwin(wsize), wsize/2, [], Fs);
-        tiledlayout(3,1)
-        ax1 = nexttile;
-        plot(F, 10*log10(Pxx));
-        xlim([0,120]);
-        title('PSD')
-        ax2 = nexttile;
-        plot((1:length(phase))/Fs,phase(iF,:));
-        hold on;
-        plot((1:length(phase))/Fs,this_filt_phase(:))
-        ax3 = nexttile;
-        plot((1:length(phase))/Fs,this_data);
-        hold on
-        plot((1:length(phase))/Fs,this_filt_data(:));
-        linkaxes([ax2 ax3], 'x');
-        dummy = 0;
+% models for "good theta"
+models = [model_all, model_no_gamma, model_gamma_theta, model_delta_theta, ...
+    model_beta_theta, model_hgamma_theta, model_lgamma_theta, model_theta];
+
+% models for "good-gamma"
+% models = [model_all, model_gamma, model_hi_gamma, model_lo_gamma, model_no_delta, ...
+%     model_no_theta, model_no_beta, model_gamma_delta, model_gamma_theta, model_gamma_beta];
+
+sspe_pack = cell(1,25);
+for iS = 1:25
+    this_data = trial_data{iS}.data(1,:);
+    this_res = cell(1, length(models));
+    for iM = 1:length(models)
+        this_model = models(iM);
+        this_cfg.freqs = all_freqs(this_model.spec);
+        this_cfg.freqBands = fstop_list(this_model.spec);
+        this_cfg.Fs = Fs;
+        this_cfg.ampVec = repmat(0.99, 1, length(this_cfg.freqs));
+        this_cfg.sigmaObs = 1;
+        this_cfg.window = round(length(this_data)*0.98);    
+        this_cfg.sigmaFreqs = all_sigmaFreqs(this_model.spec);
+        this_res{iM}.cfg_sspe = this_cfg;
+        % The magic happens here
+        [this_res{iM}.omega, this_res{iM}.phase, this_res{iM}.phase_bounds, ...
+         this_res{iM}.fullX] =  causalPhaseEM_MKmdl_temp(this_data, ...
+                                                    this_res{iM}.cfg_sspe);
     end
+    % Calculate PSD for each segment
+    wsize = 512;
+    [Pxx, F] = pwelch(this_data, rectwin(wsize), wsize/2, [], Fs);
+    % Save results to the struct
+    sspe_pack{iS}.data = this_data;
+    sspe_pack{iS}.res = this_res;
+    sspe_pack{iS}.filt_data = trial_filt{iS}(1,:,:);
+    sspe_pack{iS}.filt_phase = trial_phase{iS}(1,:,:);
+    sspe_pack{iS}.psd.Pxx = Pxx;
+    sspe_pack{iS}.psd.F = F;
+    
 end
-%%
+%% Plot results
+
+
+
+
