0251da59dd0fa8a4479772f90e9f2404ee2be2ff
Manish Mohapatra <manishofyore@gmail.com>
2022-08-17 15:03:52 -0400

    Update generate_CellWise_report.m

--- code-matlab/generate_CellWise_report.m
+++ code-matlab/generate_CellWise_report.m
@@ -1,5 +1,5 @@
 % Assumes that spike-sorting has been done
-cd('E:\Dropbox (Dartmouth College)\manish_data\M321\M321-2022-07-09');
+cd('E:\Dropbox (Dartmouth College)\manish_data\M322\M322-2022-07-19');
 LoadExpKeys;
 evs = LoadEvents([]);
 cfg_spk.min_cluster_quality = 3;
@@ -7,8 +7,8 @@ cfg_spk.uint = '64';
 S = LoadSpikes(cfg_spk);
 
 %% Set variables
-fsz1 = 8; % Text font size
-fsz2 = 10; % 
+fsz1 = 7; % Text font size
+fsz2 = 8; % 
 
 %% Remove spikes during stim times
 all_short_stim = [evs.t{strcmp(evs.label, ExpKeys.pre_trial_stim_on)}; ...
@@ -16,6 +16,11 @@ all_short_stim = [evs.t{strcmp(evs.label, ExpKeys.pre_trial_stim_on)}; ...
     evs.t{strcmp(evs.label, ExpKeys.post_trial_stim_on)}];
 
 all_long_stim = [evs.t{strcmp(evs.label, ExpKeys.long_stim_on)}];
+
+%Tske only the first 25 long stim if special stim present
+if strcmp(ExpKeys.special_stim, 'Yes')
+   all_long_stim = all_long_stim(1:25); 
+end
 to_be_removed = [all_short_stim,  all_short_stim + ExpKeys.short_stim_pulse_width; ...
     all_long_stim, all_long_stim + ExpKeys.long_stim_pulse_width];
 
@@ -27,14 +32,34 @@ restricted_S = restrict(S, clean_iv);
 
 %% Set variables and parameters
 % snippet for autocorrelation
-for iC = 1:length(restricted_S.label)   
+for iC = 1:length(restricted_S.label)
     fn_prefix = extractBefore(restricted_S.label{iC}, '.t');
     temp_idx = find(fn_prefix == '_');
     fn_prefix(temp_idx) = '-';
     
+    %Extract AD2BitVoltConversioFactor from corresponding .ntt file
+    temp_idx = find(fn_prefix == '-');
+    ntt_fn = strcat(extractBefore(fn_prefix, temp_idx(end)), '.ntt');
+    if isfile(ntt_fn)
+        [~, ~, ~, ~, ~, hdr] = Nlx2MatSpike(ntt_fn, [1 1 1 1 1], 1, 1, []);
+        temp_idx = find(contains(hdr, 'ADBitVolts'));
+        if ~ isempty(temp_idx)
+            hdr = hdr{temp_idx};
+            hdr = split(hdr, ' ');
+            conv_factors = cellfun(@str2double, hdr(2:end));
+            %Convert to microvolts
+            conv_factors = conv_factors*1e6;
+        else
+           fprintf("Something wrong with the header of %s, Skipping\n", ntt_fn);
+           continue;
+        end
+    else
+        fprintf("%s not found in the directory, Skipping\n", ntt_fn);
+        continue;
+    end
 
     this_fig = figure('WindowState', 'maximized');
-    subplot(3,4,1)
+    subplot(4,4,1)
     % Load ClusterQual
     temp_cq = load(strcat(fn_prefix, '-ClusterQual.mat'));
     text(0, 1, fn_prefix, 'FontSize', fsz1, 'Interpreter', 'none');
@@ -53,30 +78,52 @@ for iC = 1:length(restricted_S.label)
     axis off;
     
     % Plot autocorrelation
-    subplot(3,4,2)
+    subplot(4,4,2)
     cfg_xc.smooth = 1;
     [test_xc, test_t] = ccf(cfg_xc,restricted_S.t{iC},restricted_S.t{iC});
     plot(test_t, test_xc);
+    xlim([test_t(find(test_t > 0, 1)), test_t(end)])
     xlabel('Time in Seconds', 'FontSize', fsz2)
     title('Autocorrelation', 'FontSize', fsz2)
     
     % Plot logISI
-    subplot(3,4,3)
+    subplot(4,4,3)
     test_isi = diff(restricted_S.t{iC});
     histogram(log10(test_isi), -3.75:0.5:2.75);
-    xline(-3, 'r');
-    title('LogISI', 'FontSize', fsz2)
+    xline(log10(0.002), 'r');
+    this_str = sprintf(', %d/%d ISIs less than 2 msec', ...
+        sum(test_isi<0.002), length(test_isi));
+    title(strcat('LogISI', this_str), 'FontSize', fsz2)
     
     % Load average waveformfile and plot it
     subplot(3,4,4)
     temp_wv = load(strcat(fn_prefix,'-wv.mat'));
-    plot(temp_wv.mWV);
+    %Convert_Values to microvolts
+    mWV = temp_wv.mWV .* conv_factors';
+    plot(mWV);
+    ylabel('micro-Volts');
     title('Average SpikeWaveform', 'FontSize', fsz2)
+    
+    %Extract Spike amplitudes from coresponding .awv file
+    awv_fn = strcat(fn_prefix, '-awv.mat');
+    if isfile(awv_fn)
+        subplot(4,4,[5,6,7,8])
+        temp_awv = load(awv_fn);
+        temp_awv = temp_awv.WV;
+        max_awv = max(temp_awv,[],3);
+        max_awv = max_awv .* conv_factors';
+        plot(S.t{iC}, max_awv, '.', 'MarkerSize', 10)
+        ylabel('micro-Volts');
+        dummy2 = 0;
+    else
+       fprintf("All Waveforms file missing for %s, Skipping\n", fn_prefix);
+       continue
+    end
     
     this_cell = SelectTS([], S, iC);
     
     %Pre-stim raster
-    subplot(3,4,[5,9])
+    subplot(4,4,[9,13])
     this_on_events = evs.t{strcmp(evs.label, ExpKeys.pre_trial_stim_on)};
     [outputS, outputT, outputGau, outputIT, cfg] = SpikePETHvdm([], ...
         this_cell, this_on_events, '', 0.5);
@@ -91,7 +138,7 @@ for iC = 1:length(restricted_S.label)
     title('Pre-stim')
    
     %Trial-stim raster
-    subplot(3,4,[6,10])
+    subplot(4,4,[10,14])
     this_on_events = evs.t{strcmp(evs.label, ExpKeys.trial_stim_on)};
     [outputS, outputT, outputGau, outputIT, cfg] = SpikePETHvdm([], ...
         this_cell, this_on_events, '', 0.5);
@@ -106,7 +153,7 @@ for iC = 1:length(restricted_S.label)
     title('Trial-stim')
     
     %Post-stim raster
-    subplot(3,4,[7,11])
+    subplot(4,4,[11,15])
     this_on_events = evs.t{strcmp(evs.label, ExpKeys.post_trial_stim_on)};
     [outputS, outputT, outputGau, outputIT, cfg] = SpikePETHvdm([], ...
         this_cell, this_on_events, '', 0.5);
@@ -121,7 +168,7 @@ for iC = 1:length(restricted_S.label)
     title('Post-stim')
     
     %Long-stim raster
-    subplot(3,4,[8,12])
+    subplot(4,4,[12,16])
     this_on_events = evs.t{strcmp(evs.label, ExpKeys.long_stim_on)};
     [outputS, outputT, outputGau, outputIT, cfg] = SpikePETHvdm([], ...
         this_cell, this_on_events, '', 0.5);
@@ -137,7 +184,6 @@ for iC = 1:length(restricted_S.label)
     
     print(this_fig, '-dpdf', '-fillpage', strcat(fn_prefix,'-CellReport'));
     close;
-    
 end
 
 
