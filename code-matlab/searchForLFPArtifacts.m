09a48c8f4408df7b19410015640469168adf7ffd
Manish Mohapatra <manishofyore@gmail.com>
2023-11-03 21:07:19 -0400

    Update searchForLFPArtifacts.m

--- code-matlab/searchForLFPArtifacts.m
+++ code-matlab/searchForLFPArtifacts.m
@@ -30,10 +30,10 @@ function doStuff
     end
 
 %     cell_ID = 'M019-2019-04-15-TT06_1.t'; % Great oscillations
-    cell_ID = 'M019-2019-04-14-TT08_1.t'; % Great artifact
-    if isempty(find(strcmp(cell_ID, ExpKeys.goodCell)))
-       return
-    end
+%     cell_ID = 'M020-2019-06-07-TT08_1.t'; % Great artifact
+%     if isempty(find(strcmp(cell_ID, ExpKeys.goodCell)))
+%        return
+%     end
 
     fn_prefix = split(pwd, '\');
     fn_prefix = fn_prefix{end};
@@ -78,7 +78,7 @@ function doStuff
     end
     control_on = stim_on + rand(size(stim_on)) - 0.5;
 
-    w = [-.1 .1]; % time window to compute STA over
+    w =[-0.25,0.25]; % time window to compute STA over
     this_tvec = w(1):1/Fs:w(2); % time axis for STA
     [this_on_snip, this_control_snip] = deal(zeros(length(stim_on), length(this_tvec)));
     [filt_snip, phase_snip] = deal(zeros(length(fbands), length(stim_on), length(this_tvec)));
@@ -98,39 +98,47 @@ function doStuff
         this_on_toAdd = csc.data(control_start:control_start+length(this_tvec)-1);
         this_control_snip(iEvt,:) = this_on_toAdd';   
     end
-% 
-%     % Load the phases at stim_on in various frequency bands
-%     load('stim_phases.mat');
-%     % Get rid of all the 3rd band stuff IF there are 4 bands
-%     if size(causal_phase, 1) == 4 causal_phase(3,:) = []; end
-% 
-%     fig = figure('WindowState', 'Maximized');
-%     for iF = 1:length(fbands)
-%         subplot(3,3, (iF-1)*3+1)
-%         histogram(stim_phase(iF,:),phase_bins,'FaceColor',c_list{iF});
-%         title(sprintf('Hilbert %d - % d Hz',fbands{iF}(1), fbands{iF}(2)));
-% 
-%         subplot(3,3, (iF-1)*3+2)
-%         histogram(causal_phase(iF,:),phase_bins,'FaceColor',c_list{iF});
-%         title(sprintf('Causal %d - % d Hz',fbands{iF}(1), fbands{iF}(2)));
-%     end
-% 
-%     subplot(3,3,[3,6,9])
-%     plot(this_tvec, mean(this_on_snip), 'cyan');
-%     hold on
-%     plot(this_tvec, mean(this_control_snip), 'magenta');
-%     xline(0, '--black')
-%     legend({'Stim','Control'})
-%     sgtitle(fn_prefix, 'Interpreter', 'none')
-%     print(fig, '-dpng',  strcat('E:\Dropbox (Dartmouth College)\AnalysisResults\phase_stim_results\ArtifactEval\', fn_prefix, '-LFP-Report'));
-%     close;
 
+    % Load the phases at stim_on in various frequency bands
+    load('stim_phases.mat');
+    % Get rid of all the 3rd band stuff IF there are 4 bands
+    if size(causal_phase, 1) == 4 causal_phase(3,:) = []; end
+
+    fig = figure('WindowState', 'Maximized');
+    for iF = 1:length(fbands)
+        subplot(3,4, (iF-1)*4+1)
+        histogram(stim_phase(iF,:),phase_bins,'FaceColor',c_list{iF});
+        title(sprintf('Hilbert at Stim %d - % d Hz',fbands{iF}(1), fbands{iF}(2)));
+
+        subplot(3,4, (iF-1)*4+2)
+        histogram(control_phase(iF,:),phase_bins,'FaceColor',c_list{iF});
+        title(sprintf('Hilbert at Control %d - % d Hz',fbands{iF}(1), fbands{iF}(2)));
+
+        subplot(3,4, (iF-1)*4+3)
+        histogram(causal_phase(iF,:),phase_bins,'FaceColor',c_list{iF});
+        title(sprintf('Causal at Stim %d - % d Hz',fbands{iF}(1), fbands{iF}(2)));
+    end
+
+    subplot(3,4,[4,8,12])
+    plot(this_tvec, mean(this_on_snip), 'cyan');
+    hold on
+    plot(this_tvec, mean(this_control_snip), 'magenta');
+    xline(0, '--black')
+    legend({'Stim','Control'})
+    sgtitle(fn_prefix, 'Interpreter', 'none');
+    mkdir('STA_dump');
+    cd('STA_dump');
+    print(fig, '-dpng', 'stim_phase_dist' );
+    close;
     % Code to find examples of artifacts
     first = 0;
     normfun = @(x) (x - min(x))/(max(x) - min(x));
     while first < length(stim_on)
         fig = figure('WindowState', 'maximized');
         for iP = 1:5
+            if first + iP > length(stim_on)
+                break
+            end
             for iF = 1:3
                 subplot(5,3,(iP-1)*3+iF)
                 hold on
@@ -138,7 +146,7 @@ function doStuff
                 plot(this_tvec, squeeze(normfun(filt_snip(iF,first+iP,:))), 'Color', c_list{iF}, 'LineStyle', '--');
                 plot(this_tvec, squeeze(normfun(phase_snip(iF,first+iP,:))), 'Color', c_list{iF});
                 xline(0, '--black')
-                xticks([-0.1 0 0.1])
+                xticks([-0.25 0 0.25])
                 yticks([])
                 if iP == 1
                     title(sprintf('%d - %d Hz', fbands{iF}(1), fbands{iF}(2)))
@@ -146,10 +154,14 @@ function doStuff
                 if iF == 1
                     ylabel(sprintf('Stim #%d', first+iP))
                 end
+                if iF == 3
+                    xticks([-0.1 0 0.1]);
+                    xlim([-0.1 0.1]);
+                end
             end
         end
         first = first+5;
-        print(fig, '-dpng',  strcat('E:\Dropbox (Dartmouth College)\AnalysisResults\phase_stim_results\STA_dump\', num2str(first)));
+        print(fig, '-dpng',  num2str(first));
         close;
     end       
 end
